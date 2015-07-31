cf099a257cd8c7c1e039afd960434ae3d12d35d0
lintong <lintong724@163.com>
2015-07-31 15:29:57 +0800

    删除了不需要的目录

--- resource/spider_config.py
+++
@@ -1,6 +0,0 @@
-# coding:utf-8
-class SpiderConfig(object):
-    CRAWLER_INTERVAL_MIN = 4
-    CRAWLER_INTERVAL_MAX = 10
-    ZHIHU_USER = u"lintong724@163.com"
-    ZHIHU_PASSWORD = u"2XcCeP0kyLHKiGPc"
